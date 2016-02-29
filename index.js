aaa6e1bcbd207cd3362a260b9b53b365637bf3cb
Michael Smesnik <ms@tailored-apps.com>
2016-02-29 15:48:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+export const ALL_PERMISSIONS = 'all'
+export const ALL_TARGETS = 'all'
+
+export const CATEGORY_DEPLOY = 'deploy'
+
+export const MSG_LEVEL_OK = 'ok'
+export const MSG_LEVEL_INFO = 'info'
+export const MSG_LEVEL_RAW = 'raw'
+export const MSG_LEVEL_DEBUG = 'debug'
+
+export const PERMISSION_GRANT = 'grant'
+export const PERMISSION_DENY = 'deny'
+
+export const PERMISSION_CATEGORY_DEPLOY = 'deploy'
+export const PERMISSION_CATEGORY_USER_LIST = 'userList'
+export const PERMISSION_CATEGORY_USER_MANAGEMENT = 'userManagement'
+export const PERMISSION_CATEGORY_PERMISSION_MANAGEMENT = 'permissionManagement'
