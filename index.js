1c6163f7a49811a7173d82bb33e7c0549f42e48e
Michael Smesnik <ms@tailored-apps.com>
2016-02-29 16:29:12 +0100

    don't use es6 syntax without transpiling, stupid -.-

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict'
 
-export const ALL_PERMISSIONS = 'all'
-export const ALL_TARGETS = 'all'
+module.exports = {
+  ALL_PERMISSIONS: 'all',
+  ALL_TARGETS: 'all',
 
-export const CATEGORY_DEPLOY = 'deploy'
+  CATEGORY_DEPLOY: 'deploy',
 
-export const MSG_LEVEL_OK = 'ok'
-export const MSG_LEVEL_INFO = 'info'
-export const MSG_LEVEL_RAW = 'raw'
-export const MSG_LEVEL_DEBUG = 'debug'
+  MSG_LEVEL_OK: 'ok',
+  MSG_LEVEL_INFO: 'info',
+  MSG_LEVEL_RAW: 'raw',
+  MSG_LEVEL_DEBUG: 'debug',
 
-export const PERMISSION_GRANT = 'grant'
-export const PERMISSION_DENY = 'deny'
+  PERMISSION_GRANT: 'grant',
+  PERMISSION_DENY: 'deny',
 
-export const PERMISSION_CATEGORY_DEPLOY = 'deploy'
-export const PERMISSION_CATEGORY_USER_LIST = 'userList'
-export const PERMISSION_CATEGORY_USER_MANAGEMENT = 'userManagement'
-export const PERMISSION_CATEGORY_PERMISSION_MANAGEMENT = 'permissionManagement'
+  PERMISSION_CATEGORY_DEPLOY: 'deploy',
+  PERMISSION_CATEGORY_USER_LIST: 'userList',
+  PERMISSION_CATEGORY_USER_MANAGEMENT: 'userManagement',
+  PERMISSION_CATEGORY_PERMISSION_MANAGEMENT: 'permissionManagement'
+}
